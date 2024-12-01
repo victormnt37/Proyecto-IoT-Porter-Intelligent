f8aa5889e8072a70f26203645324fdab14d43407
RebecaMAy <mrmenaya@upv.edu.es>
2024-12-02 00:18:05 +0100

    dos adaptadores para edificios

---
+++ app/src/main/java/com/example/design_vicent_sprint1/data/Edificios.java
@@ -0,0 +1,36 @@
+package com.example.design_vicent_sprint1.data;
+
+import com.example.design_vicent_sprint1.model.Edificio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Edificios extends ArrayList{
+
+    private List<Edificio> edificios;
+
+    public Edificios() {
+        edificios = new ArrayList<>();
+        edificios.add(new Edificio(0));
+        //cargarEdificiosEjemplo();
+    }
+
+    public void cargarEdificio(Edificio edificio){
+        if (!edificios.contains(edificio)) {
+            edificios.add(edificio);
+        }
+    }
+
+    public List<Edificio> getEdificios() {
+        return edificios;
+    }
+
+    public Edificio getEdificioById(String id) {
+        for (Edificio edificio : edificios) {
+            if (edificio.getId().equals(id)) {
+                return edificio;
+            }
+        }
+        return null;
+    }
+}
