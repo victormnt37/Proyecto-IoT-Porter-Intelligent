0d94eef022e809c8c35e4ce9b291648c12ce08bd
victormnt37 <victormorantf@gmail.com>
2024-12-05 20:29:06 +0100

    featured tiempo display
    aunque ahora mismo solo sale no se puede obtener el clima

--- app/src/main/java/com/example/design_vicent_sprint1/model/PanelAdapter.java
+++ app/src/main/java/com/example/design_vicent_sprint1/model/PanelAdapter.java
@@ -3,21 +3,25 @@ package com.example.design_vicent_sprint1.model;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.design_vicent_sprint1.R;
+import com.example.design_vicent_sprint1.data.RepositorioWeather;
 
 import java.util.List;
 
 public class PanelAdapter extends RecyclerView.Adapter<PanelAdapter.PanelViewHolder> {
 
     private List<Panel> paneles;
+    private String edificioSeleccionado;
 
-    public PanelAdapter(List<Panel> paneles) {
+    public PanelAdapter(List<Panel> paneles, String edificioSeleccionado) {
         this.paneles = paneles;
+        this.edificioSeleccionado = edificioSeleccionado;
     }
 
     @NonNull
@@ -31,6 +35,13 @@ public class PanelAdapter extends RecyclerView.Adapter<PanelAdapter.PanelViewHol
     public void onBindViewHolder(@NonNull PanelViewHolder holder, int position) {
         Panel panel = paneles.get(position);
         holder.tipoPanel.setText(panel.getTipo());
+
+        holder.panelVacio.removeAllViews();
+
+        switch (panel.getTipo()) {
+            case "Tiempo":
+                llenarTiempo(holder, panel);
+        }
     }
 
     @Override
@@ -40,11 +51,33 @@ public class PanelAdapter extends RecyclerView.Adapter<PanelAdapter.PanelViewHol
 
     public static class PanelViewHolder extends RecyclerView.ViewHolder {
         TextView tipoPanel;
+        LinearLayout panelVacio;
 
         public PanelViewHolder(@NonNull View itemView) {
             super(itemView);
             tipoPanel = itemView.findViewById(R.id.tipoPanel);
+            panelVacio = itemView.findViewById(R.id.panelVacio);
         }
+    }
+
+    public void llenarTiempo(PanelViewHolder holder, Panel panel) {
+        RepositorioWeather RepositorioWeather = new RepositorioWeather();
+        // pedir el edificio con el edificioSeleccionado
+        //Edificio edificio = new Edificio("", "Edificio Central", "Calle Principal", "Madrid");
+        Edificio edificio = panel.getEdificio();
+
+        Weather weather = RepositorioWeather.getWeatherForEdificio(edificio);
+
+        TextView elTiempo = new TextView(holder.itemView.getContext());
+
+        // ahora mismo weather siempre es nulo
+        if (weather != null) {
+            elTiempo.setText("Estado del clima: " + weather.getWeatherStateName() + "Temperatura actual: " + weather.getTheTemp() + "°C");
+        } else {
+            elTiempo.setText("No se pudo obtener el clima.");
+        }
+
+        holder.panelVacio.addView(elTiempo);
     }
 }
 
